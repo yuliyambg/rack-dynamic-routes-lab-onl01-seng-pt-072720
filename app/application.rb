11b6c48c83dfde4ea72b1560da216f40a642fc11
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-10 21:32:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
   if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
-      item = @@items.find{|i| i.name == item_name}
+      item = @@items.find{|item| item.name == item_name}
 
       if item.nil?
         resp.write "Item not found"
@@ -19,12 +19,13 @@ class Application
       else
         resp.write item.price
       end
-    else
+   
+      else
       resp.write "Route not found"
       resp.status = 404
-    end
+      end
     
-  resp.finish
-  end
+      resp.finish
+      end
 
 end
